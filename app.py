e0faa3a5e7031f2ca07ef68e865e793155a1212d
jiaawe <99934518+jiaawe@users.noreply.github.com>
2023-08-18 02:11:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,42 @@
-from flask import Flask, request, jsonify
-from flask_restful import Resource, Api
-import numpy as np
+from flask import Flask, render_template, request
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.resnet50 import preprocess_input
-import sys
-import os
+from tensorflow.keras.utils import load_img, img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import decode_predictions
+#from keras.applications.vgg16 import VGG16
 
 app = Flask(__name__)
-api = Api(app)
-port = 5501
-
-if sys.argv.__len__() > 1:
-    port = sys.argv[1]
-print("Api running on port : {} ".format(port))
-
-# Load the trained CNN model
 model = load_model('deepfake.h5')
 
-# Define a function for image preprocessing
-def preprocess_image(img):
-    img = image.load_img(img, target_size=(224, 224))
-    img = image.img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img = preprocess_input(img)
-    return img
+@app.route('/', methods=['GET'])
+def hello_word():
+    return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/', methods=['POST'])
 def predict():
-    try:
-        # Get the uploaded image from the request
-        uploaded_file = request.files['file']
-        
-        # Preprocess the uploaded image
-        img = preprocess_image(uploaded_file)
-        
-        # Make prediction using the model
-        prediction = model.predict(img)
-        prediction_label = 'Positive' if prediction[0][0] > 0.5 else 'Negative'
-        
-        return jsonify({'prediction': prediction_label})
-    except Exception as e:
-        return jsonify({'error': str(e)})
+    imagefile= request.files['imagefile']
+    image_path = "./images/" + imagefile.filename
+    imagefile.save(image_path)
+
+    image = load_img(image_path, target_size=(224, 224))
+    image = img_to_array(image)
+    image = image/225
+    image = image.reshape((1, 224, 224, 3))
+    #image = preprocess_input(image)
+    print(image)
+    yhat = model.predict(image)
+    print(yhat[0][0])
+    label = yhat
+    # label = decode_predictions(yhat)
+    label = label[0][0]
+
+    if label > 0.5:
+        classification = 'negative: ' + str(label) 
+    else:
+        classification = 'positive: ' + str(label)
+
+    return render_template('index.html', prediction=classification)
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(port=3000, debug=True)
